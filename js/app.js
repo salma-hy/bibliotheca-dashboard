d8248639eb9a6cd315dda3b15474c9ddf8eb4eb3
salma-hy <salma.haymer@gmail.com>
2026-01-02 01:02:31 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
 function showSection(id) {
-  document.querySelectorAll('.section').forEach(sec => {
-    sec.classList.remove('active');
-  });
+  document.querySelectorAll('.section')
+    .forEach(sec => sec.classList.remove('active'));
   document.getElementById(id).classList.add('active');
 }

6f35c2d77be055dcc341af76dcba0ab6946582fb
salma-hy <salma.haymer@gmail.com>
2026-01-03 17:36:24 +0100

    Delete js directory

--- js/app.js
+++
@@ -1,5 +0,0 @@
-function showSection(id) {
-  document.querySelectorAll('.section')
-    .forEach(sec => sec.classList.remove('active'));
-  document.getElementById(id).classList.add('active');
-}
--- js/storage.js
+++
@@ -1,7 +0,0 @@
-function save(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
-
-function load(key) {
-  return JSON.parse(localStorage.getItem(key)) || [];
-}
